26512d8e412d3223ca978029981875aec318ed07
Mayank Chugh <mayankchugh1977@gmail.com>
2021-01-03 08:35:13 +0800

    more files

---
+++ Solution_Creating_Templates_with_JS/index.js
@@ -0,0 +1,3 @@
+var varTemplate = '<p>For attending our conference on Frontend Development.</p>'+'Hope all of you found it informative!!!'+
+        '<p>We look forward to see you all in future conferences.</p>'+'<p>Best Regards,</p>'+'<h2>Team UpGrad</h2>';
+        document.getElementById('addTemplate').innerHTML=varTemplate;
